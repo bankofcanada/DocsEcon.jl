e07e1f919125decf88f6c2d27e8a3f2eb1fa9855
Boyan Bejanov <bbejanov@bankofcanada.ca>
2020-08-14 20:10:21 -0400

    fixed silly bug

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,18 @@
 
 @info "Testing TimeSeriesEcon:"
+using TimeSeriesEcon
 cd(joinpath(dirname(pathof(TimeSeriesEcon)), "..", "test")) do
     include(joinpath(pwd(), "runtests.jl"))
 end
 
 @info "Testing ModelBaseEcon:"
+using ModelBaseEcon
 cd(joinpath(dirname(pathof(ModelBaseEcon)), "..", "test")) do
     include(joinpath(pwd(), "runtests.jl"))
 end
 
 @info "Testing StateSpaceEcon:"
+using StateSpaceEcon
 cd(joinpath(dirname(pathof(StateSpaceEcon)), "..", "test")) do
     include(joinpath(pwd(), "runtests.jl"))
 end
