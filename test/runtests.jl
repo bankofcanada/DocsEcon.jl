264c9da1a4a1c07193cbb6acb5f807c97de6f555
Boyan Bejanov <bbejanov@bankofcanada.ca>
2020-08-14 20:25:07 -0400

    all tests pass now

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,13 @@ cd(joinpath(dirname(pathof(ModelBaseEcon)), "..", "test")) do
     include(joinpath(pwd(), "runtests.jl"))
 end
 
+using StateSpaceEcon
+@using_example E1
+@using_example E2
+empty!(E1.model.sstate.constraints)
+empty!(E2.model.sstate.constraints)
+# clear_sstate!(E1.model)
+
 @info "Testing StateSpaceEcon:"
 using StateSpaceEcon
 cd(joinpath(dirname(pathof(StateSpaceEcon)), "..", "test")) do
