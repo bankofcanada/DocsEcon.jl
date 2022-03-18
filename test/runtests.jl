016675d1df18a9b381cafdb3aeb1628ecb8a8dd7
Boyan Bejanov <bbejanov@gmail.com>
2022-03-18 11:44:59 -0400

    using Pkg to run test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,27 @@
 
+using Pkg
 using Test
 
 @info "Testing TimeSeriesEcon:"
 using TimeSeriesEcon
-cd(joinpath(dirname(pathof(TimeSeriesEcon)), "..", "test")) do
-    include(joinpath(pwd(), "runtests.jl"))
-end
+Pkg.test("TimeSeriesEcon")
+# cd(joinpath(dirname(pathof(TimeSeriesEcon)), "..", "test")) do
+#     include(joinpath(pwd(), "runtests.jl"))
+# end
 
 @info "Testing ModelBaseEcon:"
 using ModelBaseEcon
-cd(joinpath(dirname(pathof(ModelBaseEcon)), "..", "test")) do
-    include(joinpath(pwd(), "runtests.jl"))
-end
+Pkg.test("ModelBaseEcon")
+# cd(joinpath(dirname(pathof(ModelBaseEcon)), "..", "test")) do
+#     include(joinpath(pwd(), "runtests.jl"))
+# end
 
 @info "Testing StateSpaceEcon:"
 using StateSpaceEcon
-cd(joinpath(dirname(pathof(StateSpaceEcon)), "..", "test")) do
-    include(joinpath(pwd(), "runtests.jl"))
-end
+Pkg.test("StateSpaceEcon")
+# cd(joinpath(dirname(pathof(StateSpaceEcon)), "..", "test")) do
+#     include(joinpath(pwd(), "runtests.jl"))
+# end
 
 @info "Testing Tutorials"
 @testset "US_SW07" begin
