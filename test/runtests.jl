14e48488e2a634208350d3dadce79015fed2bd2f
Boyan Bejanov <bbejanov@gmail.com>
2022-04-07 14:17:31 -0400

    test tutorials

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,8 +24,17 @@ Pkg.test("StateSpaceEcon")
 # end
 
 @info "Testing Tutorials"
+
+@testset "Time Series" begin
+    include(joinpath("..", "src", "Tutorials", "1.TimeSeriesEcon", "main.jl"))
+end
+
+@testset "Simple RBC" begin
+    include(joinpath("..", "src", "Tutorials", "2.simple_RBC", "main.jl"))
+end
+
 @testset "US_SW07" begin
-    include(joinpath("..", "src", "Tutorials", "US_SW07", "main.jl"))
+    include(joinpath("..", "src", "Tutorials", "3.US_SW07", "main.jl"))
 end
 
 @info "All done."
