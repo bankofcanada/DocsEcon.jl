00b9ef1cbef5280ae5642b19cf07d8ba774567c1
Boyan Bejanov <bbejanov@gmail.com>
2022-04-07 15:03:08 -0400

    added tutorials tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,27 +5,19 @@ using Test
 @info "Testing TimeSeriesEcon:"
 using TimeSeriesEcon
 Pkg.test("TimeSeriesEcon")
-# cd(joinpath(dirname(pathof(TimeSeriesEcon)), "..", "test")) do
-#     include(joinpath(pwd(), "runtests.jl"))
-# end
 
 @info "Testing ModelBaseEcon:"
 using ModelBaseEcon
 Pkg.test("ModelBaseEcon")
-# cd(joinpath(dirname(pathof(ModelBaseEcon)), "..", "test")) do
-#     include(joinpath(pwd(), "runtests.jl"))
-# end
 
 @info "Testing StateSpaceEcon:"
 using StateSpaceEcon
 Pkg.test("StateSpaceEcon")
-# cd(joinpath(dirname(pathof(StateSpaceEcon)), "..", "test")) do
-#     include(joinpath(pwd(), "runtests.jl"))
-# end
 
 @info "Testing Tutorials"
 
 @testset "Time Series" begin
+    @test true
     include(joinpath("..", "src", "Tutorials", "1.TimeSeriesEcon", "main.jl"))
 end
 
